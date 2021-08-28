278d6a036263ac73a8abc361888cec86cf9c059c
HanselV <hanselviva@gmail.com>
2021-08-28 15:52:50 -0700

    added styling on posts

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -14,7 +14,6 @@ import { Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-	cardWrapper: {},
 	cardGrid: {
 		paddingTop: theme.spacing(8),
 		paddingBottom: theme.spacing(8),
@@ -37,7 +36,7 @@ const Posts = () => {
 			.get("https://justpost8-api.herokuapp.com/posts")
 			.then((res) => {
 				setPosts(res.data);
-				console.log("posts", res.data);
+				// console.log("posts", res.data);
 			})
 			.catch((err) => {
 				console.log(err.response.data);
@@ -53,25 +52,25 @@ const Posts = () => {
 			<Grid container spacing={4}>
 				{posts.map((post) => {
 					return (
-						<Grid
-							className={classes.cardWrapper}
-							item
-							key={post.post_id}
-							xs={12}
-							sm={6}
-							md={4}
-						>
+						<Grid item key={post.post_id} xs={12} sm={6} md={4}>
 							<Card bordered className={classes.card}>
 								<CardContent>
-									<H5>{post.post_title}</H5>
+									<H5 style={{ fontFamily: "Questrial" }}>{post.post_title}</H5>
 									<Subtitle2 secondary style={{ marginBottom: "12px" }}>
 										Posted at: {convertDate(post.posted_at)} by {post.user_id}
 									</Subtitle2>
-									<Body1>{post.post_body}</Body1>
+									<Body1
+										style={{
+											marginTop: "20px",
+											fontFamily: "Sarabun",
+										}}
+									>
+										{post.post_body}
+									</Body1>
 								</CardContent>
-								<CardAction>
-									<Button text color="var(--primary)">
-										Learn More
+								<CardAction style={{ marginBottom: "20px" }}>
+									<Button text color="var(--primary)" outlined>
+										Visit profile
 									</Button>
 								</CardAction>
 							</Card>

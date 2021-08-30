b552e9a4957b6d6e159be82c8507ff5539a306d5
HanselV <hanselviva@gmail.com>
2021-08-30 11:27:22 -0700

    added dialog to post when user is not logged in

--- frontend/src/components/Posts/CreatePost.js
+++ frontend/src/components/Posts/CreatePost.js
@@ -1,11 +1,20 @@
-import React from "react";
-// import Button from "@material-ui/core/Button";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+//
+import { Divider, Button } from "ui-neumorphism";
+
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import BorderColorOutlinedIcon from "@material-ui/icons/BorderColorOutlined";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { Divider, Button } from "ui-neumorphism";
+//
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const useStyles = makeStyles((theme) => ({
 	createPost: {
@@ -24,10 +33,27 @@ const useStyles = makeStyles((theme) => ({
 	submit: {
 		margin: theme.spacing(3, 0, 2),
 	},
+	dialog: {},
+	dialogButton: {
+		margin: theme.spacing(2),
+	},
 }));
 
-const CreatePost = () => {
+const CreatePost = (props) => {
 	const classes = useStyles();
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = (e) => {
+		e.preventDefault();
+		if (props.isLoggedIn === false) {
+			setOpen(true);
+		}
+		//need to add else if user if logged in
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 
 	return (
 		<Container component="main" maxWidth="md" className={classes.createPost}>
@@ -54,14 +80,45 @@ const CreatePost = () => {
 						rows={5}
 					/>
 					<Divider elevated={true} />
-					<Button bordered className={classes.submit}>
+					<Button bordered className={classes.submit} onClick={handleClickOpen}>
 						<BorderColorOutlinedIcon style={{ marginRight: "10px" }} />
 						Post 8!
 					</Button>
 				</form>
 			</div>
+
+			<Dialog open={open} onClose={handleClose} className={classes.dialog}>
+				<DialogTitle>{"You are not logged in."}</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						Please login or register to post on our wall.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button
+						className={classes.dialogButton}
+						onClick={handleClose}
+						color="primary"
+					>
+						Login
+					</Button>
+					<Button
+						className={classes.dialogButton}
+						onClick={handleClose}
+						color="primary"
+						autoFocus
+					>
+						Register
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</Container>
 	);
 };
 
-export default CreatePost;
+const mapStateToProps = (state) => ({
+	isLoading: state.isLoading,
+	isLoggedIn: state.isLoggedIn,
+});
+
+export default connect(mapStateToProps, {})(CreatePost);
--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -49,7 +49,7 @@ const Profile = (props) => {
 			});
 	}, []);
 
-	//need to create onsubmit
+	//need to create onsubmit in redux
 
 	return (
 		<Container className={classes.cardGrid} maxWidth="lg">
